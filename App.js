162c0963e120e41d2ecdcb385871b2afc0504ebe
moteroeiras <motero@insideone.com.ar>
2017-11-20 19:03:34 -0300

    codePush

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import CodePush from 'react-native-code-push'
 
 export default class App extends Component<{}> {
+  constructor(props){
+    super(props);
+    this.state = {
+      status : ''
+    }
+  }
+
+  checkupdate(){
+    CodePush.sync({
+      updateDialog : true,
+      installMode: CodePush.InstallMode.INMADIATE
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -29,9 +33,7 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button title="CodePush" onPress={ ()=> this.checkupdate() }/>
       </View>
     );
   }
